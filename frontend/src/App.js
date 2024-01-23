f711b8c4aaeffcdbee8a894d7f5a530a265586b1
Talant13 <khaldarov.tala@gmail.com>
2024-01-23 15:46:53 -0800

    initial

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Book</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
