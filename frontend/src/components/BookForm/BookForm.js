306f70fb7373103ed74a3809cdfcdda2ec8c1c9e
Talant13 <khaldarov.tala@gmail.com>
2024-01-23 16:52:06 -0800

    components with styles

---
+++ frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookForm = () => {
+  return (
+    <div className="app-block book-form">
+      <h2>Form</h2>
+    </div>
+  );
+};
+
+export default BookForm;
---
+++ frontend/src/components/Filter/Filter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Filter = () => {
+  return <div className="app-block filter">Filter</div>;
+};
+
+export default Filter;
