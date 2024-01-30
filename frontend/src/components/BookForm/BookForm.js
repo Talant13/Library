d71c0369b93b0b1098fba3f6e50e8766e9f6d8ab
Talant13 <khaldarov.tala@gmail.com>
2024-01-30 15:58:07 -0800

    random Book

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import "./BookForm.css";
+import booksData from "../../data/books.json";
 import { addBook } from "../../redux/books/actionCreators";
 
 const BookForm = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const dispatch = useDispatch();
+
+  const handleAddRandomBook = () => {
+    const randomIndex = Math.floor(Math.random() * booksData.length);
+    const randomBook = booksData[randomIndex];
+    const fullBook = {
+      ...randomBook,
+      id: uuidv4(),
+    };
+
+    dispatch(addBook(fullBook));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -47,6 +59,9 @@ const BookForm = () => {
           />
         </div>
         <button type="submit">Add Book</button>
+        <button type="button" onClick={handleAddRandomBook}>
+          Random
+        </button>
       </form>
     </div>
   );
